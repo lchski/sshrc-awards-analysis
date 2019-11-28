b6fb9a8bded4a66e3145f2c83678fd31cb7b37e4
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-28 18:21:20 -0500

    some more toying
    (messy data!)

--- load.R
+++ load.R
@@ -2,7 +2,14 @@ library(tidyverse)
 
 source("scripts/helpers.R")
 
-awards <- fs::dir_ls("data/source/", regexp = "\\.csv$") %>% 
+## Data from: https://open.canada.ca/data/en/dataset/b4e2b302-9bc6-4b33-b880-6496f8cef0f1
+files <- tibble(path = fs::dir_ls("data/source/", regexp = "\\.csv$")) %>%
+  mutate(year = as.integer(str_extract(path, "[0-9]{4}"))) %>%
+  mutate(original_schema = year < 2016)
+
+awards_schema_1 <- files %>%
+  filter(original_schema) %>%
+  pull(path) %>%
   map_dfr(
     read_csv,
     .id = "source_file",
@@ -10,8 +17,22 @@ awards <- fs::dir_ls("data/source/", regexp = "\\.csv$") %>%
     col_types = cols("AwardAmount" = col_character())
   )
 
+awards_schema_2 <- files %>%
+  filter(! original_schema) %>%
+  pull(path) %>%
+  map_dfr(
+    read_csv,
+    .id = "source_file",
+    locale = locale(encoding = "ISO-8859-1")
+  )
+
+## Compare names! (hint: they differ)
+awards_schema_1 %>% names()
+awards_schema_2 %>% names()
+
 t <- awards %>% mutate(
-  AwardDollarsA = str_remove_all(AwardAmount, fixed("$")),
-  AwardDollarsB = str_remove_all(AwardDollarsA, fixed(",")),
-  AwardDollarsDbl = as.double(AwardDollarsB)
+  AwardDollars = str_remove_all(AwardAmount, "\\$|,")
+) %>% mutate(
+  AwardDollars = as.double(AwardDollars)
 )
+
